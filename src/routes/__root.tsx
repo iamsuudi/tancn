550382e40e2ecb8dc308bdb481f95e3f9b4a28a7
vijayabaskar <vj2k02@gmail.com>
2025-12-04 09:09:53 +0530

    chore: google tag added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -46,6 +46,11 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 	scripts: () => [
 		{
 			children: `
+			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KP662D4F')
 				(function() {
 					const STORAGE_KEY = "sponsor-banner-closed-at";
 					const BANNER_HIDE_DURATION = 2 * 24 * 60 * 60 * 1000; // 2 days in milliseconds
@@ -161,6 +166,8 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<HeadContent />
 			</head>
 			<body suppressHydrationWarning={true}>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KP662D4F"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 				<ThemeProvider
 					defaultTheme="system"
 					attribute="class"
